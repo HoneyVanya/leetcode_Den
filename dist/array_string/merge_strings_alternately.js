4b71b93ccbc1ff62b5f794ade4669afedb94839f
HoneyVanya <bad.batareika@gmail.com>
2025-03-13 18:35:04 +0100

    made 2 tasks, pushing for fun

---
+++ dist/array_string/merge_strings_alternately.js
@@ -0,0 +1,15 @@
+"use strict";
+// 1768. Merge Strings Alternately
+let word1 = "ab", word2 = "pqrs";
+function mergeAlternately(word1, word2) {
+    let n = '';
+    for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
+        if (word1[i])
+            n = n + word1[i];
+        if (word2[i])
+            n = n + word2[i];
+    }
+    return n;
+}
+;
+console.log(mergeAlternately(word1, word2));
---
+++ src/array_string/greatest_common_devisor_of_string.ts
@@ -0,0 +1,42 @@
+
+// 1071. Greatest Common Divisor of Strings
+
+function gcdOfStrings(str1: string, str2: string): string {
+
+    let l1 = str1.length
+    let l2 = str2.length
+    let str = ''
+    for (let i = 0; i < Math.min(l1, l2); i++) {
+        if (str1[i] === str2[i]) {
+            str = str + str1[i]
+        } 
+    } 
+
+    while (str.length > 0) {
+
+        if (l1 % str.length !== 0 || l2 % str.length !== 0) {
+            str = str.slice(0, -1)
+        } else {
+            break
+        }
+    }
+    let i = 0
+    let j = 0
+
+    while (j < Math.max(str1.length, str2.length)) {
+        console.log(j)
+
+        if (i === str.length) i = 0
+        if (str1[j] && str1[j] !== str[i] || str2[j] && str2[j] !== str[i]) {
+            return ''
+        }
+        j++
+        i++
+    }
+    return str
+}; 
+
+let str1 = "AAAAAAAAA"
+let str2 = "AAACCC"
+
+console.log(gcdOfStrings(str1, str2))
